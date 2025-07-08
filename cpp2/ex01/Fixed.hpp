78696c3f2b593a169938b2cbb34cd29c8e96faea
Philippe Menard <phil.menard1668@gmail.com>
2025-07-08 18:10:36 +0200

    finished exercice 00, starting 01

--- cpp2/ex01/Fixed.hpp
+++ cpp2/ex01/Fixed.hpp
@@ -8,11 +8,15 @@ class Fixed
 	public:
 		Fixed();
 		Fixed(Fixed const & copy);
+		Fixed(int const n);
+		Fixed(float const f);
 		~Fixed();
 		Fixed & operator=(Fixed const & rhs);
 
 		int		getRawBits() const;
 		void	setRawBits(int const raw);
+		float	toFloat() const;
+		int		toInt() const;
 
 	private:
 		int					_fixed;
