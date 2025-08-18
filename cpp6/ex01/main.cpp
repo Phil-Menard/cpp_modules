84c4d1b6b87feea32bee63f2420b4cacf908db0a
philippe <phil.menard1668@gmail.com>
2025-08-18 19:55:04 +0200

    finished exercice 01

---
+++ cpp6/ex01/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include "Serializer.hpp"
+
+int main()
+{
+    Data d;
+    d.val = 42;
+
+    uintptr_t a = Serializer::serialize(&d);
+    std::cout << a << std::endl;
+    Data* b = Serializer::deserialize(a);
+    std::cout << b->val << std::endl;
+
+    return 0;
+}
