037ba6db291e3b1642a72ed3f67d8f8353b342d0
Philippe Menard <phil.menard1668@gmail.com>
2025-08-18 17:29:45 +0200

    finished first exercice

--- cpp6/ex00/ScalarConverter.cpp
+++ cpp6/ex00/ScalarConverter.cpp
@@ -56,7 +56,9 @@ int countDigitsAfterComma(std::string const str)
 {
 	int count = 0;
 	int i = str.size() - 1;
-	while(str[i] == '0')
+	if (str[i] == 'f')
+		i--;
+	while(str[i] && str[i] == '0')
 		i--;
 	while (str[i] != '.')
 	{
@@ -68,7 +70,7 @@ int countDigitsAfterComma(std::string const str)
 	return count;
 }
 
-bool isFloat(std::string const str)
+bool isFloat(std::string const str, float & a)
 {
 	int i = 0;
 	if (str[str.size()-1] != 'f')
@@ -88,7 +90,7 @@ bool isFloat(std::string const str)
 	}
 	if (counter_dot == 0 || counter_dot > 1)
 		return false;
-	float a = 0.0f, index_dot;
+	float index_dot;
 	while (str[i] != 'f')
 	{
 		if (str[i] == '.')
@@ -100,14 +102,12 @@ bool isFloat(std::string const str)
 	a /= index_dot;
 	if (str[0] == '-')
 		a *= -1;
-	int precision = countDigitsAfterComma(str);
-	std::cout << std::fixed << std::setprecision(precision) << a << "f" << std::endl;
 	if (a > std::numeric_limits<float>::max() || a < std::numeric_limits<float>::min())
 		return false;
 	return true;
 }
 
-bool isDouble(std::string const str)
+bool isDouble(std::string const str, double & a)
 {
 	int i = 0;
 	if (str[i] == '+' || str[i] == '-')
@@ -125,7 +125,7 @@ bool isDouble(std::string const str)
 	}
 	if (counter_dot == 0 || counter_dot > 1)
 		return false;
-	double a = 0.0, index_dot;
+	double index_dot;
 	while (str[i])
 	{
 		if (str[i] == '.')
@@ -137,23 +137,136 @@ bool isDouble(std::string const str)
 	a /= index_dot;
 	if (str[0] == '-')
 		a *= -1;
-	int precision = countDigitsAfterComma(str);
-	std::cout << std::fixed << std::setprecision(precision) << a << std::endl;
 	if (a > std::numeric_limits<double>::max() || a < std::numeric_limits<double>::min())
 		return false;
 	return true;
 }
 
+void convertFromChar(std::string const str)
+{
+	std::cout << "char: ";
+	if ((str[0] >= 32 && str[0] <= 126) || (str[0] >= 9 && str[0] <= 13))
+		std::cout << "'" << str[0] << "'" << std::endl;
+	else
+		std::cout << "Non displayable" << std::endl;
+	std::cout << "int: " << static_cast<int>(str[0]) << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(str[0]) << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(str[0]) << std::endl;
+}
+
+void convertFromInt(std::string const str)
+{
+	int a = atoi(str.c_str());
+
+	std::cout << "char: ";
+	if ((a >= 32 && a <= 126) || (a >= 9 && a <= 13))
+		std::cout << "'" << static_cast<char>(a) << "'" << std::endl;
+	else
+		std::cout << "Non displayable" << std::endl;
+	std::cout << "int: " << a << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(a) << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(a) << std::endl;
+}
+
+void convertFromFloat(std::string const str, float a, bool pl)
+{
+	int precision = 1;
+
+	if (pl)
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		if (str[0] == '+')
+		{
+			std::cout << "float: " << std::fixed << std::setprecision(precision) << "+" << a << "f" << std::endl;
+			std::cout << "double: " << std::fixed << std::setprecision(precision) << "+" << static_cast<double>(a) << std::endl;
+		}
+		else
+		{
+			std::cout << "float: " << std::fixed << std::setprecision(precision) << a << "f" << std::endl;
+			std::cout << "double: " << std::fixed << std::setprecision(precision) << static_cast<double>(a) << std::endl;
+		}
+	}
+	else
+	{
+		precision = countDigitsAfterComma(str);
+		std::cout << "char: ";
+		if ((a >= 32 && a <= 126) || (a >= 9 && a <= 13))
+			std::cout << "'" << static_cast<char>(a) << "'" << std::endl;
+		else
+			std::cout << "Non displayable" << std::endl;
+		std::cout << "int: " << static_cast<int>(a) << std::endl;
+		std::cout << "float: " << std::fixed << std::setprecision(precision) << a << "f" << std::endl;
+		std::cout << "double: " << std::fixed << std::setprecision(precision) << static_cast<double>(a) << std::endl;
+	}
+}
+
+void convertFromDouble(std::string const str, double a, bool pl)
+{
+	int precision = 1;
+	if (pl)
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		if (str[0] == '+')
+		{
+			std::cout << "float: " << std::fixed << std::setprecision(precision) << "+" << static_cast<float>(a) << "f" << std::endl;
+			std::cout << "double: " << std::fixed << std::setprecision(precision) << "+" << a << std::endl;
+		}
+		else
+		{
+			std::cout << "float: " << std::fixed << std::setprecision(precision) << static_cast<float>(a) << "f" << std::endl;
+			std::cout << "double: " << std::fixed << std::setprecision(precision) << a << std::endl;
+		}
+	}
+	else
+	{
+		precision = countDigitsAfterComma(str);
+		std::cout << "char: ";
+		if ((a >= 32 && a <= 126) || (a >= 9 && a <= 13))
+			std::cout << "'" << static_cast<char>(a) << "'" << std::endl;
+		else
+			std::cout << "Non displayable" << std::endl;
+		std::cout << "int: " << static_cast<int>(a) << std::endl;
+		std::cout << "float: " << std::fixed << std::setprecision(precision) << static_cast<float>(a) << "f" << std::endl;
+		std::cout << "double: " << std::fixed << std::setprecision(precision) << a << std::endl;
+	}
+}
+
+bool isPseudoLiteral(std::string const str)
+{
+	if (str == "-inff" || str == "+inff" || str == "nanf")
+	{
+		float a = strtof(str.c_str(), NULL);
+		convertFromFloat(str, a, true);
+		return true;
+	}
+	else if (str == "-inf" || str == "+inf" || str == "nan")
+	{
+		double d = strtod(str.c_str(), NULL);
+		convertFromDouble(str, d, true);
+		return true;
+	}
+	else
+		return false;
+}
+
 void ScalarConverter::convert(std::string const str)
 {
+	float a = 0;
+	double d = 0;
+
 	if (str.size() == 1 && !std::isdigit(str[0]))
-		std::cout << "CHAR" << std::endl;
+		convertFromChar(str);
 	else if (isInt(str))
-		std::cout << "INT" << std::endl;
-	else if (isFloat(str))
-		std::cout << "FLOAT" << std::endl;
-	else if (isDouble(str))
-		std::cout << "DOUBLE" << std::endl;
+		convertFromInt(str);
+	else if (isFloat(str, a))
+		convertFromFloat(str, a, false);
+	else if (isDouble(str, d))
+		convertFromDouble(str, d, false);
 	else
-		std::cout << "The type conversion is impossible." << std::endl;
+	{
+		if (!isPseudoLiteral(str))
+			std::cout << "The type conversion is impossible." << std::endl;
+	}
 }
