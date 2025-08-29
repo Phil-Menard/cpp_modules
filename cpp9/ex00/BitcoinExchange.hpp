56e4b58ad9dde0448c61807919afe0dc0794487b
Philippe Menard <phil.menard1668@gmail.com>
2025-08-29 14:52:59 +0200

    starting cpp9

---
+++ cpp9/ex00/BitcoinExchange.hpp
@@ -0,0 +1,15 @@
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+class BitcoinExchange
+{
+	public:
+		BitcoinExchange();
+		BitcoinExchange(BitcoinExchange const & copy);
+		BitcoinExchange & operator=(BitcoinExchange const & other);
+		~BitcoinExchange();
+
+		
+};
+
+#endif
